105e83de143e674d650aec1cd5d529cc776873cd
Andrii <andrejsalogub3@gmail.com>
2024-05-23 16:03:26 +0300

    Added rules button inside Footer component.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,16 @@ import musicOn from "../../assets/images/music_sign.svg";
 import musicOff from "../../assets/images/music_off.svg";
 import soundOn from "../../assets/images/volume.svg";
 import soundOff from "../../assets/images/volume_off.svg";
+import questioMark from "../../assets/images/question_mark.svg";
+import cross from "../../assets/images/cross.svg";
+import mabelInfo from "../../assets/images/mabel-info.png";
 import { SoundContext } from "../SoundContext/SoundContext";
 import "./Footer.scss";
 
 const Footer = () => {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isRulesOpened, setIsRulesOpened] = useState(false);
   const { playSound, isSoundEnabled, toggleSound } = useContext(SoundContext);
 
   useEffect(() => {
@@ -22,7 +26,12 @@ const Footer = () => {
     } else {
       audioRef.current.play();
     }
+
     setIsPlaying(!isPlaying);
+  };
+
+  const toggleRules = () => {
+    setIsRulesOpened(!isRulesOpened);
   };
 
   return (
@@ -48,6 +57,38 @@ const Footer = () => {
             alt="volume button image"
           />
         </button>
+      </section>
+      <section className="footer__rules">
+        <button
+          className="footer__button footer__button_rules"
+          onClick={() => {
+            toggleRules(), playSound();
+          }}
+        >
+          <img
+            src={isRulesOpened ? cross : questioMark}
+            alt="rules question mark"
+          />
+        </button>
+        <div
+          className={
+            isRulesOpened
+              ? "footer__rules-info footer__rules-info_visible"
+              : "footer__rules-info"
+          }
+        >
+          <p>Don't click on the same card twice!</p>
+          <p>Click on GRAVITY FALLS logo to go back.</p>
+        </div>
+        <img
+          className={
+            isRulesOpened
+              ? "footer__rules-image footer__rules-image_visible"
+              : "footer__rules-image"
+          }
+          src={mabelInfo}
+          alt="mabel rules"
+        />
       </section>
     </footer>
   );
